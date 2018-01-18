8a75cd7323daab2e65b9b3158ffe0615653c0647
吕常龙 <417891235@qq.com>
2018-01-18 17:14:56 +0800

    logback 0.0.1

---
+++ logback-demo/src/main/java/grailslm/lvchanglong/HelloWorld.java
@@ -0,0 +1,16 @@
+package grailslm.lvchanglong;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger("grailslm.lvchanglong.HelloWorld"); //root
+        logger.trace("Hello World !!!");
+
+        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+        StatusPrinter.print(lc); //可简单理解为，查看logback实时运行状态
+    }
+}
